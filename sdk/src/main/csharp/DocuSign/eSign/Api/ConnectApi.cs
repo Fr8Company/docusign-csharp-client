c6cb82924b6fe2b8b160bcf42f739db0440d8df7
teh_netaholic <teh.netaholic@gmail.com>
2016-03-23 16:59:45 +0300

    added support for Connect API

---
+++ sdk/src/main/csharp/DocuSign/eSign/Api/ConnectApi.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+
+namespace DocuSign.eSign.Api
+{
+    public class ConnectApi
+    {
+        public Configuration Configuration { get; set; }
+
+        public ConnectApi(Configuration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public ApiResponse<ConnectInformation> GetConnect(string accountId, string connectId)
+        {
+            if (accountId == null) throw new ApiException(400, "Missing required parameter 'accountId' when calling GetEnvelope");
+
+
+            var path_ = "/v2/accounts/{accountId}/connect/{connectId}";
+
+            var pathParams = new Dictionary<String, String>();
+            var queryParams = new Dictionary<String, String>();
+            var headerParams = new Dictionary<String, String>(Configuration.DefaultHeader);
+            var formParams = new Dictionary<String, String>();
+            var fileParams = new Dictionary<String, FileParameter>();
+            String postBody = null;
+
+            // to determine the Accept header
+            String[] http_header_accepts = new String[] {
+                "application/json"
+            };
+            String http_header_accept = Configuration.ApiClient.SelectHeaderAccept(http_header_accepts);
+            if (http_header_accept != null)
+                headerParams.Add("Accept", Configuration.ApiClient.SelectHeaderAccept(http_header_accepts));
+
+            // set "format" to json by default
+            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
+            pathParams.Add("format", "json");
+            if (accountId != null) pathParams.Add("accountId", Configuration.ApiClient.ParameterToString(accountId)); // path parameter
+            if (connectId != null) pathParams.Add("connectId", Configuration.ApiClient.ParameterToString(connectId)); // path parameter
+
+            // make the HTTP request
+            IRestResponse response = (IRestResponse)Configuration.ApiClient.CallApi(path_, Method.GET, queryParams, postBody, headerParams, formParams, fileParams, pathParams);
+
+            int statusCode = (int)response.StatusCode;
+
+            if (statusCode >= 400)
+                throw new ApiException(statusCode, "Error calling GetEnvelope: " + response.Content, response.Content);
+            else if (statusCode == 0)
+                throw new ApiException(statusCode, "Error calling GetEnvelope: " + response.ErrorMessage, response.ErrorMessage);
+
+            return new ApiResponse<ConnectInformation>(statusCode,
+                response.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
+                (ConnectInformation)Configuration.ApiClient.Deserialize(response, typeof(ConnectInformation)));
+        }
+
+        public ApiResponse<ConnectInformation> ListConnects(string accountId)
+        {
+
+            if (accountId == null) throw new ApiException(400, "Missing required parameter 'accountId' when calling GetEnvelope");
+
+
+            var path_ = "/v2/accounts/{accountId}/connect";
+
+            var pathParams = new Dictionary<String, String>();
+            var queryParams = new Dictionary<String, String>();
+            var headerParams = new Dictionary<String, String>(Configuration.DefaultHeader);
+            var formParams = new Dictionary<String, String>();
+            var fileParams = new Dictionary<String, FileParameter>();
+            String postBody = null;
+
+            // to determine the Accept header
+            String[] http_header_accepts = new String[] {
+                "application/json"
+            };
+            String http_header_accept = Configuration.ApiClient.SelectHeaderAccept(http_header_accepts);
+            if (http_header_accept != null)
+                headerParams.Add("Accept", Configuration.ApiClient.SelectHeaderAccept(http_header_accepts));
+
+            // set "format" to json by default
+            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
+            pathParams.Add("format", "json");
+            if (accountId != null) pathParams.Add("accountId", Configuration.ApiClient.ParameterToString(accountId)); // path parameter
+
+            // make the HTTP request
+            IRestResponse response = (IRestResponse)Configuration.ApiClient.CallApi(path_, Method.GET, queryParams, postBody, headerParams, formParams, fileParams, pathParams);
+
+            int statusCode = (int)response.StatusCode;
+
+            if (statusCode >= 400)
+                throw new ApiException(statusCode, "Error calling GetEnvelope: " + response.Content, response.Content);
+            else if (statusCode == 0)
+                throw new ApiException(statusCode, "Error calling GetEnvelope: " + response.ErrorMessage, response.ErrorMessage);
+
+            return new ApiResponse<ConnectInformation>(statusCode,
+                response.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
+                (ConnectInformation)Configuration.ApiClient.Deserialize(response, typeof(ConnectInformation)));
+
+        }
+
+        public ApiResponse<ConnectConfiguration> UpdateConnect(string accountId, ConnectConfiguration connectConfiguration)
+        {
+            // verify the required parameter 'accountId' is set
+            if (accountId == null) throw new ApiException(400, "Missing required parameter 'accountId' when calling Update");
+
+            var path_ = "/v2/accounts/{accountId}/connect";
+
+            var pathParams = new Dictionary<String, String>();
+            var queryParams = new Dictionary<String, String>();
+            var headerParams = new Dictionary<String, String>(Configuration.DefaultHeader);
+            var formParams = new Dictionary<String, String>();
+            var fileParams = new Dictionary<String, FileParameter>();
+            String postBody = null;
+
+            // to determine the Accept header
+            String[] http_header_accepts = new String[] {
+                "application/json"
+            };
+            String http_header_accept = Configuration.ApiClient.SelectHeaderAccept(http_header_accepts);
+            if (http_header_accept != null)
+                headerParams.Add("Accept", Configuration.ApiClient.SelectHeaderAccept(http_header_accepts));
+
+            // set "format" to json by default
+            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
+            pathParams.Add("format", "json");
+            if (accountId != null) pathParams.Add("accountId", Configuration.ApiClient.ParameterToString(accountId)); // path parameter
+
+            postBody = Configuration.ApiClient.Serialize(connectConfiguration); // http body (model) parameter
+
+
+            // make the HTTP request
+            IRestResponse response = (IRestResponse)Configuration.ApiClient.CallApi(path_, Method.PUT, queryParams, postBody, headerParams, formParams, fileParams, pathParams);
+
+            int statusCode = (int)response.StatusCode;
+
+            if (statusCode >= 400)
+                throw new ApiException(statusCode, "Error calling Update: " + response.Content, response.Content);
+            else if (statusCode == 0)
+                throw new ApiException(statusCode, "Error calling Update: " + response.ErrorMessage, response.ErrorMessage);
+
+            return new ApiResponse<ConnectConfiguration>(statusCode,
+                response.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
+                (ConnectConfiguration)Configuration.ApiClient.Deserialize(response, typeof(ConnectConfiguration)));
+        }
+
+        public ApiResponse<ConnectConfiguration> CreateConnect(string accountId, ConnectConfiguration connectConfiguration)
+        {
+            // verify the required parameter 'accountId' is set
+            if (accountId == null) throw new ApiException(400, "Missing required parameter 'accountId' when calling Update");
+
+            var path_ = "/v2/accounts/{accountId}/connect";
+
+            var pathParams = new Dictionary<String, String>();
+            var queryParams = new Dictionary<String, String>();
+            var headerParams = new Dictionary<String, String>(Configuration.DefaultHeader);
+            var formParams = new Dictionary<String, String>();
+            var fileParams = new Dictionary<String, FileParameter>();
+            String postBody = null;
+
+            // to determine the Accept header
+            String[] http_header_accepts = new String[] {
+                "application/json"
+            };
+            String http_header_accept = Configuration.ApiClient.SelectHeaderAccept(http_header_accepts);
+            if (http_header_accept != null)
+                headerParams.Add("Accept", Configuration.ApiClient.SelectHeaderAccept(http_header_accepts));
+
+            // set "format" to json by default
+            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
+            pathParams.Add("format", "json");
+            if (accountId != null) pathParams.Add("accountId", Configuration.ApiClient.ParameterToString(accountId)); // path parameter
+
+            postBody = Configuration.ApiClient.Serialize(connectConfiguration); // http body (model) parameter
+
+
+            // make the HTTP request
+            IRestResponse response = (IRestResponse)Configuration.ApiClient.CallApi(path_, Method.POST, queryParams, postBody, headerParams, formParams, fileParams, pathParams);
+
+            int statusCode = (int)response.StatusCode;
+
+            if (statusCode >= 400)
+                throw new ApiException(statusCode, "Error calling Update: " + response.Content, response.Content);
+            else if (statusCode == 0)
+                throw new ApiException(statusCode, "Error calling Update: " + response.ErrorMessage, response.ErrorMessage);
+
+            return new ApiResponse<ConnectConfiguration>(statusCode,
+                response.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
+                (ConnectConfiguration)Configuration.ApiClient.Deserialize(response, typeof(ConnectConfiguration)));
+        }
+
+        public ApiResponse<bool> DeleteConnect(string accountId, string connectId)
+        {
+            // verify the required parameter 'accountId' is set
+            if (accountId == null) throw new ApiException(400, "Missing required parameter 'accountId' when calling DeleteTabs");
+
+            var path_ = "v2/accounts/{accountId}/connect/{connectId}";
+
+            var pathParams = new Dictionary<String, String>();
+            var queryParams = new Dictionary<String, String>();
+            var headerParams = new Dictionary<String, String>(Configuration.DefaultHeader);
+            var formParams = new Dictionary<String, String>();
+            var fileParams = new Dictionary<String, FileParameter>();
+            String postBody = null;
+
+            // to determine the Accept header
+            String[] http_header_accepts = new String[] {
+                    "application/json"
+                };
+            String http_header_accept = Configuration.ApiClient.SelectHeaderAccept(http_header_accepts);
+            if (http_header_accept != null)
+                headerParams.Add("Accept", Configuration.ApiClient.SelectHeaderAccept(http_header_accepts));
+
+            // set "format" to json by default
+            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
+            pathParams.Add("format", "json");
+            if (accountId != null) pathParams.Add("accountId", Configuration.ApiClient.ParameterToString(accountId)); // path parameter
+            if (connectId != null) pathParams.Add("connectId", Configuration.ApiClient.ParameterToString(connectId)); // path parameter
+
+
+            // make the HTTP request
+            IRestResponse response = (IRestResponse)Configuration.ApiClient.CallApi(path_, Method.DELETE, queryParams, postBody, headerParams, formParams, fileParams, pathParams);
+
+            int statusCode = (int)response.StatusCode;
+
+            if (statusCode >= 400)
+                throw new ApiException(statusCode, "Error calling DeleteTabs: " + response.Content, response.Content);
+            else if (statusCode == 0)
+                throw new ApiException(statusCode, "Error calling DeleteTabs: " + response.ErrorMessage, response.ErrorMessage);
+
+            return new ApiResponse<bool>(statusCode,
+                response.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
+                (statusCode == 200));
+        }
+    }
+}
---
+++ sdk/src/main/csharp/DocuSign/eSign/Model/ConnectConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuSign.eSign.Model
+{
+    public class ConnectInformation
+    {
+        public List<ConnectConfiguration> configurations { get; set; }
+        public string totalRecords { get; set; }
+    }
+
+    //https://www.docusign.com/p/RESTAPIGuide/Content/REST%20API%20References/Set%20Up%20a%20Connect%20Configuration.htm\
+
+    public class ConnectConfiguration
+    {
+        public string connectId { get; set; }
+        public string configurationType { get; set; }
+        public string urlToPublishTo { get; set; }
+        public string allowEnvelopePublish { get; set; } = "true";
+        public string enableLog { get; set; } = "true";
+        public string includeDocuments { get; set; } = "false";
+        public string includeCertificateOfCompletion { get; set; }
+        public string requiresAcknowledgement { get; set; } = "false";
+        public string signMessageWithX509Certificate { get; set; } = "false";
+        public string useSoapInterface { get; set; } = "false";
+        public string includeTimeZoneInformation { get; set; } = "false";
+        public string includeEnvelopeVoidReason { get; set; } = "true";
+        public string includeSenderAccountasCustomField { get; set; } = "true";
+        public string envelopeEvents { get; set; }
+        public string recipientEvents { get; set; }
+        public string userIds { get; set; }
+        public string allUsers { get; set; } = "true";
+        public string includeCertSoapHeader { get; set; }
+        public string includeDocumentFields { get; set; } = "true";
+        public string name { get; set; }
+        public string soapNamespace { get; set; } = "";
+
+        public ConnectConfiguration(string name, string url, string envelopeevents = "", string recipientevents = "")
+        {
+            this.name = name;
+            this.urlToPublishTo = url;
+            this.envelopeEvents = envelopeEvents;
+            this.recipientEvents = recipientEvents;
+        }
+    }
+
+
+
+
+}
